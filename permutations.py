52d3959d51027989afea8c675a189e22589aea89
Spencer Hurrle <spencer.hurrle@gmail.come>
2021-11-30 12:17:37 -0500

    Solution to Permutations from Codewars

---
+++ permutations.py
@@ -0,0 +1,28 @@
+"""
+Solution to Codewars Challenge "Permutations" in Python.
+"In this kata you have to create all permutations of an input string and remove duplicates, if present.
+This means, you have to shuffle all letters from the input in all possible orders."
+Sample input given by Codewars is 'a', 'ab', and 'aabb' ie. permutations('aabb')
+"""
+
+def permutations(string):
+    temp_arr = [string]
+    string_arr = list(string)
+    final_arr = []
+
+    if len(string_arr) <= 1:
+        return temp_arr
+
+    for j in range(len(string_arr)):
+        for i in range(len(string_arr)):  # ------------------------------ Here I start shifting the characters.
+            string_arr[j], string_arr[i] = string_arr[i], string_arr[j]  # Swap the characters at indexes i & j
+            temp_arr.append(''.join(string_arr))  #------------------------Join chars as a string and append
+                                                                         # our new character combination into an array
+    for x in temp_arr:
+        if x not in final_arr:
+            final_arr.append(x) #Adding elements from temp_arr that are not already in my final_arr
+
+    return final_arr
+
+
+print(permutations('aabb'))
